5770ba9904e41f06c438d85feb23c4a9ececebcf
phoenix <sahidankhan7991@gmail.com>
2022-10-18 13:41:57 -0500

    button hover

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,11 @@
 const Home = ()=>{
     return(
-        <div>
-           <h3>Personalize your charcter and embark on an exciting journey! Keep track of your progress with friends or go solo! Start Here!</h3>
+        <div >
+           <h1 className="home">Start Your Adventure!</h1>
+           <h3 className="home">Personalize your charcter and embark on an exciting journey! Keep track of your progress with friends or go solo! Start Here!</h3>
+
+           <button>Let's Begin</button>
+           
         </div>
     )
 }
--- src/pages/Nav.jsx
+++ src/pages/Nav.jsx
@@ -3,15 +3,14 @@ const Nav = ()=>{
         <div className="nav">
         <header>
           <h1 className="nav-1">Great Adventures</h1>
-
+         <button className="btn">Sign Up</button>
+         <button className="btn1">Sign In</button>
           <input
              className="search"
              type="text"
              placeholder="Search" 
               />
-
-          <h4 className="nav-2">Town</h4>
-          <h4 className="nav-2">New Player</h4>
+         
         </header>
         </div>
     )
