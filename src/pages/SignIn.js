b9ca6f8d85cbad09af78b7f4c758d39689e7c82a
phoenix <sahidankhan7991@gmail.com>
2022-12-08 06:58:48 -0600

    changed color

--- src/pages/SignIn.js
+++ src/pages/SignIn.js
@@ -1,7 +1,25 @@
 const signIn = ()=>{
 
     return(
-        <div></div>
+        <div className="background">
+            <div className="box">
+                <div className="signIn">
+               <h1>Sign In</h1>
+               <input
+               type="text"
+               placeholder="UserName"
+               name="username"/>
+               <input
+               type="password"
+               placeholder="Password"
+               name="password"/>
+               </div>
+               <div className="signUp">
+                 <h3>Not A Member? Sign Up!</h3>
+                 <button>Register</button>
+               </div>
+            </div>
+        </div>
     )
 }
 
