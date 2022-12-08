18713fbe7a4ff9bbf1a41d84e1c45807ecfd72eb
phoenix <sahidankhan7991@gmail.com>
2022-12-08 05:49:02 -0600

    first page 2/3

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,10 +1,11 @@
 const Home = ()=>{
     return(
         <div >
-           <h1 className="home">Start Your Adventure!</h1>
-           <h3 className="home">Personalize your charcter and embark on an exciting journey! Keep track of your progress with friends or go solo! Start Here!</h3>
+           <h1 className="home1">Start Your Adventure!</h1>
+           <h3 className="home2">Personalize your charcter and embark on an exciting journey! Keep track of your progress with friends or go solo! Start Here!</h3>
+           <img className="map" src="https://i.pinimg.com/564x/1a/9d/66/1a9d66c0c8fd8cf06f45e1d99a1fd2da.jpg" alt="map"/>
 
-           <button>Let's Begin</button>
+           <button className="btn2">Let's Begin</button>
            
         </div>
     )
