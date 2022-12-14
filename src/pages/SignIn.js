003d24ffcd10f7c5e2cc071cf29884f38f39a2e9
phoenix <sahidankhan7991@gmail.com>
2022-12-14 12:37:36 -0600

    idea of where sign in should go

--- src/pages/SignIn.js
+++ src/pages/SignIn.js
@@ -3,20 +3,23 @@ const signIn = ()=>{
     return(
         <div className="background">
             <div className="box">
-                <div className="signIn">
-               <h1>Sign In</h1>
-               <input
+                <div>
+               <h1 className="signIn">Sign In</h1>
+               <input 
+               className="username"
                type="text"
                placeholder="UserName"
                name="username"/>
                <input
+               className="username"
                type="password"
                placeholder="Password"
                name="password"/>
+               <button className="btnSignIn">Sign In</button>
                </div>
                <div className="signUp">
                  <h3>Not A Member? Sign Up!</h3>
-                 <button>Register</button>
+                 <button className="btnSignUp">Register</button>
                </div>
             </div>
         </div>
